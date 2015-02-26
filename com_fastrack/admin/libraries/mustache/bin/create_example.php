a37e1057256f136381f31d65a3f6cee92e5f5e63
Craig Rayner <craig@hillrange.com.au>
2015-02-27 09:42:15 +1100

    Mustache License Notification
    Signed-off-by: Craig Rayner <craig@hillrange.com.au>

--- com_fastrack/admin/libraries/mustache/bin/create_example.php
+++ com_fastrack/admin/libraries/mustache/bin/create_example.php
@@ -1,5 +1,32 @@
 #!/usr/bin/env php
 <?php
+/**
+ * Fastrack Reader/Viewer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fastrack Reader/Viewer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GiCalReader.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package		Fastrack
+ * @author		Hill Range Services http://fastrack.hillrange.com.au
+ * @copyright	Copyright (C) 2014  Hill Range Services  All rights reserved.
+ * @license		http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+defined('_JEXEC') or die();
+/*
+ * This file is part of Mustache.php. *
+ * (c) 2010-2014 Justin Hileman
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 /**
  * A commandline script to create an example and the needed files:
--- com_fastrack/admin/libraries/mustache/src/Mustache/Loader/ArrayLoader.php
+++ com_fastrack/admin/libraries/mustache/src/Mustache/Loader/ArrayLoader.php
@@ -1,8 +1,27 @@
 <?php
 
-/*
- * This file is part of Mustache.php.
+/**
+ * Fastrack Reader/Viewer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
+ * Fastrack Reader/Viewer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GiCalReader.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package		Fastrack
+ * @author		Hill Range Services http://fastrack.hillrange.com.au
+ * @copyright	Copyright (C) 2014  Hill Range Services  All rights reserved.
+ * @license		http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+defined('_JEXEC') or die();
+/*
+ * This file is part of Mustache.php. *
  * (c) 2010-2014 Justin Hileman
  *
  * For the full copyright and license information, please view the LICENSE
